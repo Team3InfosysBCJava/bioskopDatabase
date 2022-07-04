ad621f7d4ccb3c155dd8b3640a8161c5d42e4489
ZainalAbiddin <salahuddinade@gmail.com>
2022-07-04 09:12:01 +0700

    rapikan ddl - dml

--- insert_studio.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO bioskop.studio
-(studio_id, studio_name)
-VALUES('1', 'studio A');
-INSERT INTO bioskop.studio
-(studio_id, studio_name)
-VALUES('2', 'studio B');
-INSERT INTO bioskop.studio
-(studio_id, studio_name)
-VALUES('3', 'studio C');
